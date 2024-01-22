fef84293ad27bb87e73bc367a783a2125f23fd0c
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-01-22 07:58:17 +0100

    update user service to receive role and not send password

--- src/main/java/com/cypherfund/campaign/user/dto/Enumerations.java
+++ src/main/java/com/cypherfund/campaign/user/dto/Enumerations.java
@@ -2,10 +2,29 @@ package com.cypherfund.campaign.user.dto;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Enumerations {
     public enum USER_ROLES {
-        CUSTOMER, ADMIN, MANAGER, SPECTATOR, DISTRIBUTOR
+        CUSTOMER, ADMIN, SUPER_ADMIN, DISTRIBUTOR, TALENT, CLIENT, TUTOR, STUDENT;
+        private static final Set<String> ROLE_NAMES = new HashSet<>();
+
+        static {
+            for (USER_ROLES role : USER_ROLES.values()) {
+                ROLE_NAMES.add(role.name());
+            }
+        }
+
+        public static boolean areAllRolesPresent(List<String> roles) {
+            for (String role : roles) {
+                if (!ROLE_NAMES.contains(role)) {
+                    return false; // Role not found
+                }
+            }
+            return true; // All roles found
+        }
     }
     public enum USER_STATUS {
         active, blocked, suspended, disabled
