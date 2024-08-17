4da447f2d7d5e8412473d0b8d0c1dec9976e5890
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-08-17 09:38:15 +0100

    update

--- src/main/java/com/cypherfund/campaign/user/dal/entity/TUser.java
+++ src/main/java/com/cypherfund/campaign/user/dal/entity/TUser.java
@@ -68,7 +68,7 @@ public class TUser implements Serializable {
     @Column(name = "str_login_provider", length = 15)
     private String strLoginProvider;
 
-    @Size(max = 100)
+    @Size(max = 255)
     @Column(name = "img_url", length = 100)
     private String imgUrl;
 
