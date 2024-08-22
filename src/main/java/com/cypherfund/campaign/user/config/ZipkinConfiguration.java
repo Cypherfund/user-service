e24f244fdaf8d982e0dc62ed3b7192da390e63be
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-08-22 07:01:29 +0100

    fix rest template

--- src/main/java/com/cypherfund/campaign/user/config/ZipkinConfiguration.java
+++ src/main/java/com/cypherfund/campaign/user/config/ZipkinConfiguration.java
@@ -1,14 +1,14 @@
-//package com.cypherfund.campaign.user.config;
-//
-//import org.springframework.boot.web.client.RestTemplateBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.client.RestTemplate;
-//
-//@Configuration(proxyBeanMethods = false)
-//public class ZipkinConfiguration {
-//    @Bean
-//    RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        return builder.build();
-//    }
-//}
+package com.cypherfund.campaign.user.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration(proxyBeanMethods = false)
+public class ZipkinConfiguration {
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
