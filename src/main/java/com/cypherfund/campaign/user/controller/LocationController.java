b37185bc2fcd542b7c90ce1d43ced9cd469dfffa
E.Ngai <elizabeth.ngai@dme.systems>
2024-09-04 14:48:59 +0100

    add logs

--- src/main/java/com/cypherfund/campaign/user/controller/LocationController.java
+++ src/main/java/com/cypherfund/campaign/user/controller/LocationController.java
@@ -4,9 +4,11 @@ import com.cypherfund.campaign.user.model.ApiResponse;
 import com.cypherfund.campaign.user.model.LocationResponse;
 import com.cypherfund.campaign.user.services.LocationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Date: 9/4/2024
  * Time: 1:55 PM
  **/
+@Slf4j
 @RequestMapping("/locations")
 @RequiredArgsConstructor
 public class LocationController {
@@ -23,5 +26,12 @@ public class LocationController {
     public ResponseEntity<ApiResponse<LocationResponse>> getLocation(@PathVariable String ipAddress) {
         return ResponseEntity.ok(new ApiResponse<>(true, "", locationService.getLocation(ipAddress)));
     }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<LocationResponse>> getLocationWithoutIp(@RequestHeader("X-Forwarded-For") String ipAddress){
+        log.info("IP Address from X-Forwarded-For: {}", ipAddress);
+        return ResponseEntity.ok(new ApiResponse<>(true, "", locationService.getLocation(ipAddress)));
+    }
+
 
 }
