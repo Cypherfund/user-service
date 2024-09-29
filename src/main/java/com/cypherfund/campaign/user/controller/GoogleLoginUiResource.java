f15950023f6f695cca135976f14ead95168f432c
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-09-29 19:51:11 +0100

    update

--- src/main/java/com/cypherfund/campaign/user/controller/GoogleLoginUiResource.java
+++ src/main/java/com/cypherfund/campaign/user/controller/GoogleLoginUiResource.java
@@ -5,11 +5,9 @@ import com.cypherfund.campaign.user.model.GoogleSignupRequest;
 import com.cypherfund.campaign.user.model.JwtAuthenticationResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequestMapping("/oauth2")
 public interface GoogleLoginUiResource {
     @PostMapping("/authorize/google")
